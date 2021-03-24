5517f6d3a52c5cf58f7d57cea81a667ef7ecc051
Alex Sobiek <alex@alexsobiek.com>
2021-03-24 01:37:05 -0500

    Created reset() method

--- src/com/alexsobiek/SpaceRace/Window.java
+++ src/com/alexsobiek/SpaceRace/Window.java
@@ -10,15 +10,17 @@ import java.util.ArrayList;
 
 public class Window extends JPanel implements Listener {
 
-    private JFrame frame = null;
+    KeyManager keyManager = new KeyManager();
+
+    private static JFrame frame = null;
 
     public static final int winHeight = 800;
     public static final int winWidth = 1200;
     public static final int halfY = Window.winHeight / 2;
     public static final int halfX = Window.winWidth / 2;
-    public static final int maxStars = 100;
+    public static final int maxStars = 150;
 
-    java.util.List<Star> stars = new ArrayList<>();
+    private static final java.util.List<Star> stars = new ArrayList<>();
 
     public Window() {
         SpaceRace.EVENT_BUS.subscribe(this);
@@ -28,18 +30,25 @@ public class Window extends JPanel implements Listener {
         frame.setBackground(Color.BLACK);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.addKeyListener(keyManager);
         spawnStars();
-
+        Timer.start(15);
     }
 
-    private void spawnStars() {
+    public static void reset() {
+        stars.clear();
+        spawnStars();
+        Timer.start(15);
+        frame.repaint();
+    }
+
+    private static void spawnStars() {
         for (int i = 0; i <= maxStars; i++) stars.add(new Star());
     }
 
     @EventHandler
     public void onTick(GameTickEvent event) {
         if (frame != null) {
-            Timer.handleTick();
             moveStars();
             frame.repaint();
         }
@@ -66,9 +75,18 @@ public class Window extends JPanel implements Listener {
         g2d.setColor(Color.WHITE);
         stars.forEach(star -> {
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2d.fillOval(star.getX(), star.getY(), 10, 10);
+            g2d.fillOval(star.getX(), star.getY(), 6, 6);
         });
-        Timer.drawTimer(g2d);
-
+        if (GameManager.isRunning()) Timer.drawTimer(g2d);
+        else {
+            drawCenteredString(g, "Game Over!");
+        }
     }
+
+    private void drawCenteredString(Graphics g, String string) {
+        int width = (int) g.getFontMetrics().getStringBounds(string, g).getWidth();
+        int height = (int) g.getFontMetrics().getStringBounds(string, g).getHeight();
+        g.drawString(string, (halfX - (width / 2)), (halfY - (height /2)));
+    }
+
 }
