9580a2e72f8af23ba43a2efcf019dd0548f39401
Alex Sobiek <alex@alexsobiek.com>
2021-03-24 16:39:20 -0500

    Fixed & greatly improved timer

--- src/com/alexsobiek/SpaceRace/Timer.java
+++ src/com/alexsobiek/SpaceRace/Timer.java
@@ -4,54 +4,42 @@ import com.alexsobiek.SpaceRace.event.EventHandler;
 import com.alexsobiek.SpaceRace.event.Listener;
 import com.alexsobiek.SpaceRace.event.events.GameTickEvent;
 
-import java.awt.Graphics2D;
-import java.awt.BasicStroke;
+import java.awt.*;
 
 public class Timer implements Listener {
 
-    public static int time;
-    public static int tickTime;
-    public static int timeLeft;
+    // Timer
+    private static final int timerStroke = 10;
+    private static double timerHeight;
+    private static int maxTimerHeight;
 
-    private static int pixelUpdateTicks = -200;
-    private static int timeUpdateTicks = 200;
-    private static int timerHeight = Window.halfY;
-    private static int ticksUntilPixelUpdate;
+    private static double pixelsPerTick;
 
     public Timer() {
         SpaceRace.EVENT_BUS.subscribe(this);
     }
 
     public static void start(int time) {
-        System.out.println("Starting timer with " + time + " seconds...");
-        if (time%2 == 0) time++; // Time must be odd for proper math to take place.
-        tickTime = time * TickManager.TPS; // 15 seconds in ticks
-        timeLeft = time;
-        ticksUntilPixelUpdate = TickManager.TPS / (Window.halfY / time);
-    }
-
-    public static int getTimeLeft() {
-        return timeLeft;
+        int totalTicks = time * TickManager.TPS;
+        timerHeight = 0;
+        maxTimerHeight = Window.winHeight - (timerStroke * 4);
+        pixelsPerTick = (double) maxTimerHeight / totalTicks;
     }
 
     @EventHandler
     public void onTick(GameTickEvent event) {
-        if (pixelUpdateTicks == ticksUntilPixelUpdate) {
-            timerHeight++;
-            pixelUpdateTicks = 0;
-        }
-        if (timeUpdateTicks == TickManager.TPS) { // 1 second has passed
-            timeLeft--;
-            if (timeLeft == 0) GameManager.end();
-            timeUpdateTicks = 0;
-        }
-        pixelUpdateTicks++;
-        timeUpdateTicks++;
+        timerHeight += pixelsPerTick;
+        if (maxTimerHeight > 0 && timerHeight >= maxTimerHeight) GameManager.end();
     }
 
     public static void drawTimer(Graphics2D g2d) {
-        g2d.setStroke(new BasicStroke(10));
-        g2d.drawLine(Window.halfX, Window.winHeight, Window.halfX, timerHeight);
-        g2d.drawString("Time Remaining: " + timeLeft, 10, Window.winHeight - 50);
+        g2d.setColor(Color.WHITE);
+        g2d.setStroke(new BasicStroke(timerStroke));
+        g2d.drawLine(Window.halfX, 0, Window.halfX, Window.winHeight);
+
+        g2d.setColor(Color.BLACK);
+        g2d.setStroke(new BasicStroke(timerStroke + 1));
+        g2d.drawLine(Window.halfX, 0, Window.halfX, (int) timerHeight);
+        g2d.setColor(Color.WHITE);
     }
 }
