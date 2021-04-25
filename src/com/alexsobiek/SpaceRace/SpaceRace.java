b3024a0cba9e4fbb702f1a45727693111ee4a9f3
Alex Sobiek <alex@alexsobiek.com>
2021-04-24 22:00:08 -0500

    Cleaned up file

--- src/com/alexsobiek/SpaceRace/SpaceRace.java
+++ src/com/alexsobiek/SpaceRace/SpaceRace.java
@@ -7,7 +7,7 @@ import com.alexsobiek.SpaceRace.graphics.Window;
 
 /**
  * SpaceRace:
- * Written for COMP 170 Loyola University Chicago
+ * Written for COMP 170 Spring 2021 Loyola University Chicago
  *
  * @author Alexander Sobiek
  * @version 1.0
@@ -24,49 +24,10 @@ public class SpaceRace {
     public static void main(String[] args) {
         System.out.println("Starting Space Race");
         EVENT_BUS = new EventBus();
-        //new TestClass();
-
 
         GameManager.start();
         new Window();
         new KeyInput();
         new WindowResize();
-
     }
-
-//    public static final class TestClass implements Listener {
-//        private final List<Long> avgList = new ArrayList<>();
-//
-//        public TestClass() {
-//            SpaceRace.EVENT_BUS.subscribe(this);
-//            for (int i = 0; i < 1000001; i++) {
-//                SpaceRace.EVENT_BUS.post(new TestEvent(System.currentTimeMillis()));
-//                if (i + 1 == 1000001) {
-//                    System.out.println(formatOutput(avgList));
-//                    System.out.println("Average over " + i + " events: " + avgList.stream().mapToLong(a -> a).average().getAsDouble() + "ms");
-//                    break;
-//                }
-//            }
-//        }
-//
-//        @EventHandler
-//        public void onTest(TestEvent event) {
-//            avgList.add(System.currentTimeMillis() - event.getCreateTime());
-//        }
-//
-//        private String formatOutput(List<Long> results) {
-//            StringBuilder builder = new StringBuilder();
-//            int overCount = 0;
-//            for (long result : results) {
-//                overCount++;
-//                if (overCount == 40) {
-//                    builder.append("\n").append(result).append(", ");
-//                    overCount = 0;
-//                } else {
-//                    builder.append(result).append(", ");
-//                }
-//            }
-//            return builder.toString();
-//        }
-//    }
 }
